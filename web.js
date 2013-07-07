d235c078431b354d3940a8352d091d9ac258dd0d
Ubuntu <ubuntu@ip-10-242-194-164.ec2.internal>
2013-07-07 00:21:08 +0000

    bug fix

--- web.js
+++ web.js
@@ -7,7 +7,6 @@ var app = express.createServer(express.logger());
 fs.readFile('index.html', function (err, data) { 
 if (err) throw err;
   index_html = data.toString('utf-8'); 
-  // console.log(data);
 });
 
 app.get('/', function(request, response) {
