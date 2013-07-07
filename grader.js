8edcb21c1c3411b57c0945008ec1c215c4e84398
Ubuntu <ubuntu@ip-10-242-194-164.ec2.internal>
2013-07-07 12:15:42 +0000

    add url support

--- grader.js
+++ grader.js
@@ -24,7 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
+var URLFILE_DEFAULT = "url.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -65,8 +67,26 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to url.html', clone(assertFileExists), URLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var checkJson;
+ 
+    if (program.file) {
+    	checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    if (program.url) {
+	rest.get(program.url).on('complete', function(result) {
+  	if (result instanceof Error) {
+    		sys.puts('Error: ' + result.message);
+    		this.retry(5000); // try again after 5 sec
+  	} else {
+    		// sys.puts(result);
+		fs.writeFileSync(URLFILE_DEFAULT, result);
+  	}
+	});
+    	checkJson = checkHtmlFile(URLFILE_DEFAULT, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
